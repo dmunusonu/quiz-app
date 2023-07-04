79b10a6bd304a883af4917e7a3bf570bdd2d77c6
DEBENDRA KUMAR MALLICK <115154630+dmunusonu@users.noreply.github.com>
2023-07-04 16:02:42 +0530

    javascript modified

---
+++ js/questions.js
@@ -0,0 +1,103 @@
+let questions = [
+    {
+        numb:1,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:2,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:3,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:4,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:5,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:6,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:7,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:8,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:9,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+    {
+        numb:10,
+        question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        options :[
+            "4 hours",
+            "7 hours",
+            "8 hours",
+            "14 hours"     
+       ]
+    },
+   
+]
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const start_btn = document.querySelector('.start_btn button');
+ const start_btn = document.querySelector('.start_btn button');
 const info_box = document.querySelector('.info_box')
 
-const  exit_btn = info_box.querySelector('.buttons .quit');
+ const  exit_btn = info_box.querySelector('.buttons .quit');
 const continue_btn = info_box.querySelector('.buttons .restart');
-const quiz_box = document.querySelector('.quiz.box');
-const result_box = document.querySelector('result_box');
-const option_list = document.querySelector('option_list');
+const quiz_box = document.querySelector('.quiz_box');
+const result_box = document.querySelector('.result_box');
+const option_list = document.querySelector('.option_list');
 const time_line = document.querySelector('header .time_line');
 const timeText = document.querySelector('.timer .time_left');
 const timeCount = document.querySelector('.timer .timer_sec');
@@ -23,6 +23,58 @@ quiz_box.classList.add('activeQuiz');
 showQuestions(0);
 queCounter(1);
 startTimer(10);
-startTimer(0);
+startTimerLine(0);
 
+}
+
+let timeValue = 10;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector('.buttons .restart')
+const quit_quiz = result_box.querySelector('.buttons .quit');
+
+restart_quiz.onclick=()=>{
+    quiz_box.classList.add('activeQuiz');
+    result_box.classList.remove('activeResult');
+    timeValue=15;
+    que_count=0;
+    que_numb=1;
+    userScore=0;
+    widthValue=0;
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    clearInterval(counterLine);
+    startTimer(timeValue);
+    startTimerLine(widthValue);
+    timeText.textContent = 'Time Left';
+    next_btn.classList.remove('show');
+}
+const next_btn = document.querySelector('footer .next_btn');
+const bottom_ques_counter = document.querySelector('footer .total_que');
+
+next_btn.onclick = ()=>{
+    if(que_count< questions.length -1){
+        que_count++;
+        que_numb++;
+        showQuestions(que_count)
+        queCounter(que_numb);
+        clearInterval(counter);
+    clearInterval(counterLine);
+    startTimer(timeValue);
+    startTimerLine(widthValue);
+    timeText.textContent = 'Time Left';
+    next_btn.classList.remove('show');
+    }
+    else{ clearInterval(counter);
+        clearInterval(counterLine);
+        showResult();
+    }
+       
+    
 }
