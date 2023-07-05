c764a05f84666a857b2de82bb3456ac427cce9e5
DEBENDRA KUMAR MALLICK <115154630+dmunusonu@users.noreply.github.com>
2023-07-05 16:51:30 +0530

    javascript changes done here

--- js/questions.js
+++ js/questions.js
@@ -2,6 +2,7 @@ let questions = [
     {
         numb:1,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -12,6 +13,7 @@ let questions = [
     {
         numb:2,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -22,6 +24,7 @@ let questions = [
     {
         numb:3,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -32,6 +35,7 @@ let questions = [
     {
         numb:4,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -42,6 +46,7 @@ let questions = [
     {
         numb:5,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -52,6 +57,7 @@ let questions = [
     {
         numb:6,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -62,6 +68,7 @@ let questions = [
     {
         numb:7,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -72,6 +79,7 @@ let questions = [
     {
         numb:8,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -82,6 +90,7 @@ let questions = [
     {
         numb:9,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
@@ -92,6 +101,7 @@ let questions = [
     {
         numb:10,
         question : "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in:",
+        answer : "7 hours",
         options :[
             "4 hours",
             "7 hours",
